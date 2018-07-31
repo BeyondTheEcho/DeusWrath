909081c829bffa7a66833c30cd8dfdc872e51fc7
Stelios Klapsis <SteliosKlapsis@outlook.com>
2018-07-31 02:07:25 -0400

    Adding all the work done on the bot up until this point. It largely mirrors the tutorial code of the Discord.NET Library documentation. It should in this state be stable and able to carry out the Ping! - Pong! command module test.

---
+++ DeusWrath/ConfigHandler.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeusWrath
+{
+    public class ConfigHandler
+    {
+        private Config conf;
+        private string configPath;
+
+        struct Config
+        {
+            public string token;
+        }
+
+        public ConfigHandler()
+        {
+            conf = new Config()
+            {
+                token = ""
+            };
+        }
+
+        public async Task PopulateConfig()
+        {
+            configPath = Path.Combine(Directory.GetCurrentDirectory(), "config.json").Replace(@"\", @"\\");
+            Console.WriteLine(configPath);
+
+            if(!File.Exists(configPath))
+            {
+                using (StreamWriter sw = File.AppendText(configPath))
+                {
+                    sw.WriteLine(JsonConvert.SerializeObject(conf));
+                }
+
+                Console.WriteLine("WARNING! New Config Created! Need to fill in values before running commands!");
+                throw new Exception("NO CONFIG AVAILABLE! Go to executable path and fill out newly created config file");
+            }
+
+            using (StreamReader reader = new StreamReader(configPath))
+            {
+                    conf = JsonConvert.DeserializeObject<Config>(reader.ReadLine());
+            }
+
+            await Task.CompletedTask;
+
+        }
+
+        public string GetToken()
+        {
+            return conf.token;
+        }
+    }
+}
